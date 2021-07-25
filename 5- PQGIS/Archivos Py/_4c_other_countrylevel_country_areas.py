02acdbb071f25a67d86386de0cecaffad89d3d61
Matias Harari <85884882+matias-harari@users.noreply.github.com>
2021-07-25 18:58:23 -0300

    correciones de 4_c

--- 5- PQGIS/Archivos Py/_4c_other_countrylevel_country_areas.py
+++ 5- PQGIS/Archivos Py/_4c_other_countrylevel_country_areas.py
@@ -1,39 +1,39 @@
-#########################################################################################
-#########################################################################################
-# SETUP PREAMBLE FOR RUNNING STANDALONE SCRIPTS.
-# NOT NECESSARY IF YOU ARE RUNNING THIS INSIDE THE QGIS GUI.
-# print('preliminary setup')
-# import sys
-# import os
+##Objetivo: Obtener áreas de todos los países
 
-# from qgis.core import (
-#     QgsApplication,
-#     QgsVectorLayer,
-#     QgsCoordinateReferenceSystem
-# )
+#Importacion de librerias
+print('preliminary setup')
+import sys
+import os
 
-# from qgis.analysis import QgsNativeAlgorithms
+from qgis.core import (
+     QgsApplication
+ )
 
-# # See https://gis.stackexchange.com/a/155852/4972 for details about the prefix 
-# QgsApplication.setPrefixPath('C:/OSGeo4W64/apps/qgis', True)
-# qgs = QgsApplication([], False)
-# qgs.initQgis()
+ from qgis.analysis import QgsNativeAlgorithms
+ 
+#Ruta de la ubicacion de qgis 
+ QgsApplication.setPrefixPath('C:/OSGeo4W64/apps/qgis', True)
+ qgs = QgsApplication([], False)
+ qgs.initQgis()
 
-# # Add the path to Processing framework  
-# sys.path.append('C:/OSGeo4W64/apps/qgis/python/plugins')
+#Ruta del marco de procesamiento
+sys.path.append('C:/OSGeo4W64/apps/qgis/python/plugins')
 
-# # Import and initialize Processing framework
-# import processing
-# from processing.core.Processing import Processing
-# Processing.initialize()
-# QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
-#########################################################################################
+# Importar e iniciar marco de procesamiento
+import processing
+from processing.core.Processing import Processing
+Processing.initialize()
+QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
 #########################################################################################
 
-# Setear directorios de inputs y outputs
-mainpath = "/Users/magibbons/Desktop/Herramientas/Clase5/input" #carpeta de inputs
-admin_in = "{}/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp".format(mainpath) #input
-areas_out = "{}/_output/country_areas.csv".format(mainpath)#output
+# Setear carpetas de inputs a partir de objeto
+mainpath = "/Users/magibbons/Desktop/Herramientas/Clase5/input" #carpeta donde estan los inputs
+
+#Input
+admin_in = "{}/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp".format(mainpath) #input de shapefile de paises
+
+#Output
+areas_out = "{}/_output/country_areas.csv".format(mainpath)#output de areas como csv
 
 #A continuacion veremos que muchas de las operaciones que hacemos se realizan a 
 #traves de la funcion processing.run(). Es una funcion cuyo primer parametro 
@@ -59,14 +59,13 @@ crs_wcea = QgsCoordinateReferenceSystem('ESRI:54034')
 #para eliminar los campos que se detallan en dropfields. Se busca eliminar
 #todos los campos excepto los mencionados en keepfields
 
-##################################################################
 print('dropping unnecessary fields')
 
 # making a layer so we can get all attribute fields
 worldlyr = QgsVectorLayer(admin_in, 'ogr')
 allfields = [field.name() for field in worldlyr.fields()]
 keepfields = ['ne_10m_adm', 'ADMIN', 'ISO_A3'] #lista de campos 
-#for para mantener los campos que no estan en keepfields ya que son los que
+#forma para mantener los campos que no estan en keepfields ya que son los que
 #queremos eliminar luego
 dropfields = [field for field in allfields if field not in keepfields]
 
@@ -75,34 +74,31 @@ drop_dict = {
     'INPUT': admin_in,#base de estados de la que partimos
     'OUTPUT': 'memory:'#guardamos en la memoria
 }
-countries_drop_fields = processing.run('qgis:deletecolumn', drop_dict)['OUTPUT']
+countries_drop_fields = processing.run('qgis:deletecolumn', drop_dict)['OUTPUT'] #guardamos el objeto
 
 ##################################################################
 # Reproject layer
-##################################################################
 print('projecting to world cylindical equal area')
 reproj_dict = {
     'INPUT': countries_drop_fields,#partimos de base recien generada
     'TARGET_CRS': crs_wcea, #CRS elegida
     'OUTPUT': 'memory:' #guardamos en la memoria
 }
-countries_reprojected = processing.run('native:reprojectlayer', reproj_dict)['OUTPUT']
+countries_reprojected = processing.run('native:reprojectlayer', reproj_dict)['OUTPUT']#guardamos el objeto
 
 ##################################################################
 # Fix geometries
 #Corregimos geometrias
-##################################################################
 print('fixing geometries')
 fixgeo_dict = {
     'INPUT': countries_reprojected,#partimos de base recien generada
     'OUTPUT': 'memory:'#guardamos en la memoria
 }
-countries_fix_geo = processing.run('native:fixgeometries', fixgeo_dict)['OUTPUT']
+countries_fix_geo = processing.run('native:fixgeometries', fixgeo_dict)['OUTPUT']#guardamos el objeto
 
 ##################################################################
 # Field calculator, output to csv
 #Calculamos el area del poligono y exportamos la base como csv
-##################################################################
 print('calculating country areas')
 fcalc_dict = {
     'FIELD_LENGTH': 10,
